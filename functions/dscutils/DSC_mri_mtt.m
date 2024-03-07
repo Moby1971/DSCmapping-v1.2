a8e109289b83c0f0d9968d567f7562787370678e
Gustav Strijkers <g.j.strijkers@amsterdamumc.nl>
2024-03-07 11:08:49 +0100

    Initial commit

--- functions/dscutils/DSC_mri_mtt.m
+++
@@ -1,9 +0,0 @@
-function [mtt]=DSC_mri_mtt(cbv,cbf,options)
-
-deconv_method = fieldnames(cbf);
-
-for method=1:size(deconv_method,1)
-    eval(['mtt.' deconv_method{method,:} '=cbv./cbf.'  deconv_method{method,:} '.map;']);
-end
-
-end
--- functions/dscutils/DSC_mri_ttp.m
+++
@@ -1,8 +0,0 @@
-function [ttp]=DSC_mri_ttp(conc,mask,options)
-
-[~,ttp]=max(conc,[],4);
-
-ttp(not(logical(mask))) = 0;
-
-end
-
